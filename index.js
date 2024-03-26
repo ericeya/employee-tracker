29a2c9e56e8fbd46cca9bd98389b1b01a15ce829
Eric Lee <eric.hyunil.lee@gmail.com>
2024-03-26 11:02:39 -0700

    Updated basic structure of the index.js file for functionality

--- index.js
+++ index.js
@@ -1,13 +1,59 @@
 const { Pool } = require('pg');
+const inquirer = require('inquirer')
 
 const pool = new Pool(
     {
-      // TODO: Enter PostgreSQL username
       user: 'postgres',
-      // TODO: Enter PostgreSQL password
       password: 'password',
       host: 'localhost',
       database: 'movie_db'
     },
     console.log(`Connected to the movie_db database.`)
   ) 
+
+function initialMenu() {
+    inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Quit'],
+                name: 'list'
+            }
+        ])
+        .then((data)=>{
+            if (data.list === 'View All Employees') {
+                viewAllEmployees()
+            } else if (data.list === 'Add Employee') {
+                addEmployee()
+            } else if (data.list === 'Update Employee Role') {
+                updateEmployeeRole()
+            } else if (data.list === 'View All Roles') {
+                viewAllRoles()
+            } else if (data.list === 'Add Role') {
+                addRole()
+            } else if (data.list === 'View All Departments') {
+                viewAllDepartments()
+            } else if (data.list === 'Add Department') {
+                addDepartment()
+            } else if (data.list === 'Quit') {
+                return 'Have a nice day!'
+            } 
+
+        })
+
+}
+
+function viewAllEmployees() {}
+
+function addEmployee() {}
+
+function updateEmployeeRole() {}
+
+function viewAllRoles() {}
+
+function addRole() {}
+
+function viewAllDepartments() {}
+
+function addDepartment() {}
