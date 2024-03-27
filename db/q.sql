69ef51f4e0a1a0a9e47a8d28b48a1b2d722ae9a4
Eric Lee <eric.hyunil.lee@gmail.com>
2024-03-26 17:43:53 -0700

    Added pool query functions that will allow view tables for all three tables

---
+++ db/q.sql
@@ -0,0 +1,14 @@
+SELECT  e.id, e.first_name, e.last_name, title
+, department.name as department, salary, m.first_name || ' ' || m.last_name  AS manager FROM employee e
+JOIN role
+ON role_id = role.id
+JOIN department
+ON department_id = department.id
+LEFT JOIN employee m
+ON m.id = e.manager_id;
+
+SELECT r.id, r.title, d.name as department, r.salary
+FROM role r
+JOIN department d
+ON department_id = d.id;
+
--- index.js
+++ index.js
@@ -44,16 +44,59 @@ function initialMenu() {
 
 }
 
-function viewAllEmployees() {}
+function viewAllEmployees() {
+    pool.query(`SELECT  e.id, e.first_name, e.last_name, title
+    , department.name as department, salary, m.first_name || ' ' || m.last_name  AS manager FROM employee e
+    JOIN role
+    ON role_id = role.id
+    JOIN department
+    ON department_id = department.id
+    LEFT JOIN employee m
+    ON m.id = e.manager_id;`, (err, { rows }) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+          return;
+        }
+        res.json({
+          message: 'success',
+          data: rows
+        });
+      })
+}
 
 function addEmployee() {}
 
 function updateEmployeeRole() {}
 
-function viewAllRoles() {}
+function viewAllRoles() {
+    pool.query(`SELECT r.id, r.title, d.name as department, r.salary
+    FROM role r
+    JOIN department d
+    ON department_id = d.id;`, (err, { rows }) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+          return;
+        }
+        res.json({
+          message: 'success',
+          data: rows
+        });
+      })
+}
 
 function addRole() {}
 
-function viewAllDepartments() {}
+function viewAllDepartments() {
+    pool.query(`SELECT * FROM department;`, (err, { rows }) => {
+        if (err) {
+          res.status(500).json({ error: err.message });
+          return;
+        }
+        res.json({
+          message: 'success',
+          data: rows
+        });
+      })
+}
 
 function addDepartment() {}
